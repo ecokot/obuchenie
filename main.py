9bffb61403fb31fa16c0f544e6d7ea4406ca5c83
Anatoly Sushkevich <kot76_87@mail.ru>
2026-09-03 22:43:55 +0700

    изменение плана и настройка логирования
    Заменить хардкод имени приложения на загрузку из .env и добавить .gitignore.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from app.core import App, Config
 from app.core.plugins import PluginManager
 from plugins.auth import AuthPlugin
+from dotenv import load_dotenv
 import logging
+import os
+
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(name)s - %(message)s")
 
-config = Config(app_name="FRMWQwen", debug=True)
+load_dotenv() # load .env file
+config = Config(app_name=os.getenv("APP_NAME", "MiniPlatform"), debug=True)
 app = App(config)
 plugin_manager = PluginManager([AuthPlugin()], app)
 
