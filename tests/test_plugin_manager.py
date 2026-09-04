a01c02ef22c194a4af73b5fd364f28c9d2b02f45
Anatoly Sushkevich <kot76_87@mail.ru>
2026-09-04 12:22:48 +0700

    tests 2

--- tests/test_plugin_manager.py
+++ tests/test_plugin_manager.py
@@ -7,8 +7,13 @@ class FakePlugin:
         self.registered = False
 
     def register(self, app):
-        ...  # ТВОЙ КОД: что изменить, чтобы тест заметил вызов?
+        self.registered = True  # ТВОЙ КОД: что изменить, чтобы тест заметил вызов?
 
 def test_load_plugins_calls_register():
+    config = Config()
+    app = App(config)
+    plugin_manager = PluginManager([FakePlugin()], app)
+    plugin_manager.load_plugins()
+    assert plugin_manager.plugins[0].registered == True
     ...  # ТВОЙ КОД: создай App, FakePlugin, PluginManager,
     # вызови load_plugins() и проверь assert, что register был вызван
