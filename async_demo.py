68e39e69b42e6cac10bd0de674266a9e3f5e8403
Anatoly Sushkevich <kot76_87@mail.ru>
2026-09-04 15:11:29 +0700

    asyncio 2

--- async_demo.py
+++ async_demo.py
@@ -7,21 +7,29 @@ async def task(name):
     print(f"{name}: end")
 
 
+# async def main():
+#     print("Sequential:")
+#     await task("A")
+#     await task("B")
+#
+#     print("---")
+#     print("Gather (parallel):")
+#     await asyncio.gather(task("A"), task("B"))
+#
+#     print("---")
+#     print("Create_task (parallel):")
+#     t1 = asyncio.create_task(task("A"))
+#     t2 = asyncio.create_task(task("B"))
+#     await t1
+#     await t2
+#
+#
+# asyncio.run(main())
+
 async def main():
-    print("Sequential:")
-    await task("A")
-    await task("B")
-
-    print("---")
-    print("Gather (parallel):")
-    await asyncio.gather(task("A"), task("B"))
-
-    print("---")
-    print("Create_task (parallel):")
+    print("Start:")
     t1 = asyncio.create_task(task("A"))
     t2 = asyncio.create_task(task("B"))
-    await t1
-    await t2
-
+    await asyncio.sleep(1.5)
 
 asyncio.run(main())
