615c9039ba15cffc65e898e698a259a84844d6b4
Anatoly Sushkevich <kot76_87@mail.ru>
2026-09-09 23:09:08 +0700

    Реализовать интеграцию менеджера плагинов и обновить план
    Настроить инициализацию и установку PluginManager в приложении.
    Заменить заглушки запуска и остановки на вызов методов менеджера
    плагинов с проверкой состояния. Отметить этап жизненного цикла в
    плане разработки как выполненный.

--- app/core/app.py
+++ app/core/app.py
@@ -8,6 +8,7 @@ class App:
         self.config = config
         self.extensions = {}
         self.plugins = []
+        self.plugin_manager = None
 
     def add_plugin(self, plugin):
         if plugin not in self.plugins:
@@ -15,8 +16,18 @@ class App:
         else:
             logger.warning(f"Plugin {plugin.__class__.__name__} already added. Skipping...")
 
+    def set_plugin_manager(self, pm):
+        if self.plugin_manager is None:
+            self.plugin_manager = pm
+        else:
+            raise RuntimeError("PluginManager already set")
+
     def start(self):
-        ...
+        if self.plugin_manager is None:
+            raise RuntimeError("PluginManager not set")
+        self.plugin_manager.startup()
 
     def stop(self):
-        ...
+        if self.plugin_manager is None:
+            raise RuntimeError("PluginManager not set")
+        self.plugin_manager.shutdown()
