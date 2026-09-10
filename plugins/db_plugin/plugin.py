05dadb044657bcff5039051f9b91d793a139409e
Anatoly Sushkevich <kot76_87@mail.ru>
2026-09-10 21:52:11 +0700

    Настроить создание и очистку engine в плагине DBPlugin
    Инициализировать engine и sessionmaker при регистрации плагина.
    Передать фабрику сессий в приложение через register_extension.
    Вызвать dispose для engine при завершении работы плагина.

--- plugins/db_plugin/plugin.py
+++ plugins/db_plugin/plugin.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 @dataclass
 class DBConfig:
@@ -11,14 +14,19 @@ class DBPlugin:
     def __init__(self, config: DBConfig):
         self.config = config
         self.app = None
+        self.engine = None
 
     def register(self, app):
         if self.app is not None:
             raise RuntimeError("Plugin is already registered with an app")
         self.app = app
+        self.engine = create_engine(self.config.connection_string, echo=self.config.echo)
+        sm = sessionmaker(bind=self.engine)
+        app.register_extension('db_session_factory', sm)
 
     def startup(self):
         ...
 
     def shutdown(self):
-        ...
+        if self.engine is not None:
+            self.engine.dispose()
